fa3fbffdcecce75e3a1fdec345e8813a5ca46e2b
Sergio <sergioivu@gmail.com>
2018-08-09 15:00:54 -0600

    Characters search functionality added

--- src/containers/characterlist/index.js
+++ src/containers/characterlist/index.js
@@ -11,10 +11,13 @@ class CharacterList extends React.Component{
         this.state = {
             characters : [],
             offset : 0,
-            isLoading: true
+            isLoading: true,
+            total : 0,
+            searchParam : ''
         }
 
         this.fetchCharacters = this.fetchCharacters.bind(this);
+        this.searchCharacters = this.searchCharacters.bind(this);
     }
 
     render(){
@@ -24,8 +27,17 @@ class CharacterList extends React.Component{
         else
         return (
           <div className='container'>
-            <input type='button' onClick={this.fetchCharacters.bind(this, -20)} disabled={!this.state.offset > 0} value='Prev.' />
-            <input type='button' onClick={this.fetchCharacters.bind(this, 20)} value='Next' />
+            <div className="row">
+              <input type='button' onClick={this.fetchCharacters.bind(this, -20)} disabled={!this.state.offset > 0} value='Prev.' />
+              <input type='text'
+                value={this.state.searchParam}
+                onChange={(event) => { this.setState({searchParam: event.target.value})} }
+                placeholder='Search...' />
+
+              <input type='button' onClick={this.searchCharacters} value='Search' />
+                
+              <input type='button' onClick={this.fetchCharacters.bind(this, 20)} disabled={(this.state.total-this.state.offset < 20)} value='Next' />
+            </div>
             <div className='row'>
               {
                 this.state.characters.map( e =>
@@ -47,7 +59,22 @@ class CharacterList extends React.Component{
                     offset: offset});
         fetch(api+'&offset='+offset)
             .then(response => response.json())
-            .then(data => this.setState({characters : data.data.results, isLoading : false}));
+            .then(data => this.setState({characters : data.data.results,
+                                         isLoading : false,
+                                         total : data.data.total}));
+    }
+    
+    searchCharacters(){
+        this.setState({isLoading : true});
+        if (this.state.searchParam != '')
+            fetch(api+'&nameStartsWith='+this.state.searchParam)
+                .then(response => response.json())
+                .then(data => this.setState({characters : data.data.results,
+                                            isLoading : false,
+                                            total : data.data.total,
+                                            offset: 0}));
+        else
+        this.fetchCharacters(0);
     }
 
 }
